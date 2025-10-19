31671c989419343c29f122443182a8fa256e4c08
brunoestrella <brunogamer2002@gmail.com>
2025-10-18 23:17:08 -0300

    <feat: changing login and logout to CBVs>

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from accounts.forms import RegisterForm
+from django.views import View
+from accounts.forms import RegisterForm, LoginForm
 
 
 '''def register_view(request):
@@ -27,20 +28,30 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')
 
 
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+class SignInView(View):
+    template_name = 'login.html'
+    form_class = AuthenticationForm
+
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
             return redirect('cars_list')
-        else:
-            login_form = AuthenticationForm()
-    else:
-        login_form = AuthenticationForm()
+        return render(request, self.template_name, {'form': form})
+        
 
-    return render(request, 'login.html', {'login_form': login_form})
+class LogoutView(View):
+    
+
+    def get(self, request):
+        logout(request)
+        return redirect('cars_list')
 
 
 def logout_view(request):
